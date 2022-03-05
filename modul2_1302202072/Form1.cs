097a4e1334b6595084ffda81bf712a344a23b4a6
razqyaqil <razqyaqil2@gmail.com>
2022-03-05 16:11:06 +0700

    menambahkan layout GUI kalkulator

--- modul2_1302202072/Form1.cs
+++ modul2_1302202072/Form1.cs
@@ -16,5 +16,10 @@ namespace modul2_1302202072
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
